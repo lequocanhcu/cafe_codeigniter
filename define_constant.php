cca3177243d9c594ae6ecd640b4acee561804092
tranninhitw <46769619+tranninhitw@users.noreply.github.com>
2019-01-19 23:16:58 +0700

    Add files via upload

---
+++ define_constant.php
@@ -0,0 +1,14 @@
+<?php
+/************ Database ************/
+
+define('CMS_DB_HOST', 'localhost');
+define('CMS_DB_USER', 'root');
+define('CMS_DB_PASSWORD', '');
+define('CMS_DB_NAME', 'cafe2');
+define('CMS_DB_PREFIX', 'cms_');
+
+/************ ORTHER ************/
+define('CMS_BASE_URL', 'http://localhost/www/cafe/');
+define('CMS_DEFAULT_LANGUAGE', 'vietnamese');
+define('CMS_PREFIX', md5('CMS_'));
+define('COOKIE_EXPIRY', 60480);
